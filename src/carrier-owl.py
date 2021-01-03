0541eec5030e8e9ebcd56a6e8220dd156c11b79a
mrkmakr <31700156+mrkmakr@users.noreply.github.com>
2021-01-03 23:13:46 +0900

    Update carrier-owl.py

--- src/carrier-owl.py
+++ src/carrier-owl.py
@@ -21,7 +21,7 @@ warnings.filterwarnings('ignore')
 def get_articles_info(subject):
     weekday_dict = {0: 'Mon', 1: 'Tue', 2: 'Wed', 3: 'Thu',
                   4: 'Fri', 5: 'Sat', 6: 'Sun'}
-    url = f'https://arxiv.org/list/{subject}/pastweek?show=100000'
+    url = f'https://arxiv.org/list/{subject}/pastweek?show=10000'
     response = requests.get(url)
     html = response.text
     year = datetime.date.today().year
